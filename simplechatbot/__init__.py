f6e4c44c81358ef8081d7e8d85372640a61b00fc
devinjcornell <devinjcornell@github.com>
2024-10-04 09:46:57 +0200

    adding correct imports into main module

--- simplechatbot/__init__.py
+++ simplechatbot/__init__.py
@@ -1,1 +1,10 @@
+
+# whichever chatbot is here will be imported into 
+#   the simplechatbot primary namespace
 from .v4 import *
+
+# importing any other module here will dump it 
+#   into the primary namespace
+
+from .tools import *
+
