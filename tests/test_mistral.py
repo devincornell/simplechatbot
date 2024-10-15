2167e23f1d6aceb1d0d4416230ec6a283d751d17
devinjcornell <devinjcornell@github.com>
2024-10-15 14:28:11 -0400

    adding mistral test!

---
+++ tests/test_mistral.py
@@ -0,0 +1,54 @@
+
+
+from __future__ import annotations
+
+import typing
+import dataclasses
+
+import langchain_core.tools
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_mistralai import ChatMistralAI
+import pydantic
+import json
+
+#pip install duckduckgo-search
+from langchain_community.tools import DuckDuckGoSearchResults
+
+import sys
+sys.path.append('..')
+
+
+import importlib
+#import simplechatbot.v4
+#importlib.reload(simplechatbot.v4)
+import simplechatbot
+
+
+if __name__ == '__main__':
+
+    # keychain is now just a dict subclass
+    keychain = simplechatbot.devin.APIKeyChain.from_json_file('../keys.json')
+
+    system_prompt = '''
+    You are designed to answer any question the user has, to the best of your ability.
+    At some point, the user may ask you to save or retrieve text to or from a workspace.
+    You should ONLY do this if the user explicitly asks you to do so.
+    '''
+    chatbot = simplechatbot.devin.ChatBot.from_model(
+        model = ChatMistralAI(
+            model = 'mistral-large-latest',
+            api_key=keychain['mistral'],
+            system_prompt=system_prompt,
+        ),
+        system_prompt=system_prompt,
+        toolkits = [simplechatbot.tools.WorkspacesToolkit()], # just includes the rag tool from above
+    )
+    
+
+    print('=============== Starting Chat ===================\n')
+    
+    chatbot.ui.start_interactive(stream=True, show_intro=True, show_tools=True)
+
+    print('=============== Chat Ended ===================')
+
