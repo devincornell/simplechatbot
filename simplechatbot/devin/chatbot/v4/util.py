50fa508ae4e00ab504b5fa5caa0b73fa04df42b6
devinjcornell <devinjcornell@github.com>
2024-12-24 14:12:19 -0500

    removing old v4 settings

--- simplechatbot/devin/chatbot/v4/util.py
+++
@@ -1,8 +0,0 @@
-
-def format_tool_text(tool_info: dict[str|dict]) -> str:
-    '''Convenience method to format the function call to text.'''
-    name = tool_info['name']
-    args = tool_info['args']
-
-    argtext = ', '.join(f"{k}={v}" for k,v in args.items())
-    return f'{name}({argtext})'
